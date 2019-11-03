29d27ef2a76e74644297af4b584d3f4f0ee3cc79
David Mendoza <d.mendoza99@unitec.edu>
2019-11-03 11:47:26 -0600

    google eslint y prettier listos

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Hello extends Component {
   state = {
     counter: 0,
-  }
+  };
 
   increment() {
     this.setState({
-      counter: this.state.counter + 1
+      counter: this.state.counter + 1,
     });
   }
 
