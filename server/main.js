b65af29c508ddc6b3f38ac094560946b0a11ad5b
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2019-11-04 21:59:11 -0600

    Done with router config

--- server/main.js
+++ server/main.js
@@ -1,32 +1,5 @@
 import { Meteor } from "meteor/meteor";
-import Links from "/imports/api/links";
-
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink("Do the Tutorial", "https://www.meteor.com/tutorials/react/creating-an-app");
 
-    insertLink("Follow the Guide", "http://guide.meteor.com");
-
-    insertLink("Read the Docs", "https://docs.meteor.com");
-
-    insertLink("Discussions", "https://forums.meteor.com");
-  }
-});
-
-Meteor.publish('links.all', () => (
-  Links.find()
-));
-
-Meteor.methods({
-  insertJohnDoe (objectFromClient) {
-    console.log(objectFromClient);
-    if (2+2===4) {
-      Links.insert(objectFromClient);
-    }
-  }
 });
