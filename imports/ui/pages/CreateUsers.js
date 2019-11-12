d219cc91daa0f5392d592deb3f3f1af37606e6b3
marvinestrada21 <marvinestrada2@gmail.com>
2019-11-11 22:59:11 -0600

    create users page

--- imports/ui/pages/CreateUsers.js
+++ imports/ui/pages/CreateUsers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+/*
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +13,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+*/
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox,
+        Link, Grid, Box, LockOutlinedIcon, Typography, makeStyles, Container } 
+    from '@material-ui/core'
 
 function Copyright() {
   return (
