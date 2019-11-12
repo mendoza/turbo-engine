c3a3fade571d49aefdc6a33b4f13dcea27ca2008
carlosvhz <carlosvhz@outlook.com>
2019-11-12 13:15:11 -0600

    Ningun cambio

--- imports/ui/pages/Users.js
+++ imports/ui/pages/Users.js
@@ -3,14 +3,25 @@ import { Link } from "react-router-dom";
 import { Paper, Container, Card } from "@material-ui/core";
 import ChartistGraph from "react-chartist";
 
-
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Grid from '@material-ui/core/Grid';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems, secondaryListItems } from './listItems';
+import Chart from './Chart';
+import Deposits from '../layouts/Deposits';
+import Orders from '../layouts/Orders';
 
 
 export default class Users extends Component {
