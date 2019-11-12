173f249a1ade77cc4ae32f58e1f1ef3ee8d91f2e
David Mendoza <d.mendoza99@unitec.edu>
2019-11-11 20:38:35 -0600

    error 404 feo pero existe

--- imports/ui/Index.js
+++ imports/ui/Index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import AppBar from "./layouts/AppBarLayout";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Routes from "./Routes";
+import Error404 from "./pages/Error404";
 class Index extends PureComponent {
   constructor(props) {
     super(props);
@@ -17,7 +18,7 @@ class Index extends PureComponent {
             {Routes.map(route => {
               return <Route exact path={route.path} component={route.component} />;
             })}
-            {/* <Route component={Error404} /> */}
+            <Route component={Error404} />
           </Switch>
         </AppBar>
       </BrowserRouter>
