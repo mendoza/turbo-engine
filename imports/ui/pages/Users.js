038732e960fcb4369f16a0e313d145ecae709ec6
carlosvhz <carlosvhz@outlook.com>
2019-11-12 08:32:08 -0600

    test

--- imports/ui/pages/Users.js
+++ imports/ui/pages/Users.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Paper, Container } from "@material-ui/core";
+import { Paper, Container, Typography } from "@material-ui/core";
 
 export default class Users extends Component {
   render() {
     return (
       <Container>
         <Paper style={{ height: "100vh" }}>
-          wenas este es el home :v
-          <Link to="/about"> go to about</Link>
+        
         </Paper>
       </Container>
     );
