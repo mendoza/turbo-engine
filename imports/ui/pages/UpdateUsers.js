c001d280023da75b2a06ebcab1e350f9610bd989
leonard1on <leonardborj@gmail.com>
2019-11-19 16:17:37 -0600

    Snackbar en actualizar

--- imports/ui/pages/UpdateUsers.js
+++ imports/ui/pages/UpdateUsers.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React, { PureComponent } from "react";
-import { Container, Button, Typography, Grid, TextField } from "@material-ui/core";
-import { InputLabel, Select, MenuItem } from "@material-ui/core";
+import { Container, Button, IconButton, Grid, TextField,
+   Snackbar, Select, MenuItem } from "@material-ui/core";
 import { Meteor } from "meteor/meteor";
 import { withTracker } from "meteor/react-meteor-data";
 import validator from "validator";
@@ -17,6 +17,8 @@ class UpdateUsers extends PureComponent {
       apellido: "",
       correo: "",
       services: "",
+      open: "",
+      message: "",
     };
     if (props.location.state !== undefined) {
       const { user } = props.location.state;
@@ -30,6 +32,12 @@ class UpdateUsers extends PureComponent {
     }
   }
 
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
   handleTextChange = (event, stateVariable) => {
     this.setState({
       [stateVariable]: event.target.value,
@@ -39,7 +47,7 @@ class UpdateUsers extends PureComponent {
   handleChange = (event, stateVariable) => {
     let { users } = this.props;
     users = users.filter(user => user._id === event.target.value);
-    console.log(users);
+    //console.log(users);
     this.setState({
       id: users[0]._id,
       correo: users[0].emails[0].address,
@@ -47,38 +55,56 @@ class UpdateUsers extends PureComponent {
       apellido: users[0].profile.lastName,
       services: users[0].services,
     });
-    console.log(users[0].services);
+    //console.log(users[0].services);
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { id, nombre, apellido, correo, services } = this.state;
-    Meteor.call("updateUsers", {
-      _id: id,
-      emails: [
-        {
-          address: correo,
-          verified: false,
+  handleClick = () => {
+    const { id, nombre, apellido, correo, services,} = this.state;
+    let alert;
+
+    if (validator.isEmpty(nombre) === true) {
+      alert = "El campo nombre es requerido";
+    }
+    if (validator.isEmpty(apellido) === true) {
+      alert = "El campo apellido es requerido";
+    }
+    if (validator.isEmail(correo) === false) {
+      alert = "El campo correo es requerido";
+    }
+    if(alert){
+      this.setState({
+        open:true,
+        message:alert,
+      })
+    }else{
+      Meteor.call("updateUsers", {
+        _id: id,
+        emails: [
+          {
+            address: correo,
+            verified: false,
+          },
+        ],
+        services,
+        profile: {
+          firstName: nombre,
+          lastName: apellido,
+          role: "empleado",
         },
-      ],
-      services,
-      profile: {
-        firstName: nombre,
-        lastName: apellido,
-        role: "empleado",
-      },
-    });
-    alert("Usuario actualizado exitosamente");
-    this.setState({
-      nombre: "",
-      apellido: "",
-      correo: "",
-      services: "",
-    });
+      });
+      this.setState({
+        nombre: "",
+        apellido: "",
+        correo: "",
+        services: "",
+        open: true,
+        message: "Usuario actualizado exitosamente",
+      });
+  }
   };
 
   render() {
-    const { id, nombre, apellido, correo } = this.state;
+    const { id, nombre, apellido, correo, open, message } = this.state;
     const { users } = this.props;
     return (
       <DashboardLayout>
@@ -98,7 +124,13 @@ class UpdateUsers extends PureComponent {
                     if (user) {
                       return (
                         <MenuItem key={user._id} value={user._id}>
-                          {user._id} - {user.profile.firstName} {user.profile.lastName}
+                          {user._id} 
+                          {' '}
+                          -
+                          {' '} 
+                          {user.profile.firstName} 
+                          {' '}
+                          {user.profile.lastName}
                         </MenuItem>
                       );
                     }
@@ -118,7 +150,7 @@ class UpdateUsers extends PureComponent {
                   autoFocus
                   value={nombre}
                   onInput={event => this.handleTextChange(event, "nombre")}
-                />
+                  />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -132,7 +164,7 @@ class UpdateUsers extends PureComponent {
                   autoFocus
                   value={apellido}
                   onInput={event => this.handleTextChange(event, "apellido")}
-                />
+                  />
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -145,14 +177,38 @@ class UpdateUsers extends PureComponent {
                   autoComplete="email"
                   value={correo}
                   onInput={event => this.handleTextChange(event, "correo")}
-                />
+                  />
               </Grid>
-              <Button type="submit" fullWidth variant="contained" color="primary">
+              <Button 
+                type="submit" 
+                fullWidth 
+                variant="contained" 
+                color="primary" 
+                onClick={this.handleClick}
+                >
                 Actualizar
               </Button>
             </Grid>
           </form>
         </Container>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          ContentProps={{
+            "aria-describedby": "message-id",
+          }}
+          message={<span id="message-id">{message}</span>}
+          action={[
+            <IconButton key="close" aria-label="close" color="inherit" onClick={this.handleClose}>
+              <i className="fas fa-times" />
+            </IconButton>,
+          ]}
+          />
       </DashboardLayout>
     );
   }
