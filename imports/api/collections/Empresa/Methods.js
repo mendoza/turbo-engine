e5f7fc520ce4c8617bbdd48519ab9916a68c0af5
David Mendoza <d.mendoza99@unitec.edu>
2019-11-24 13:58:29 -0600

    datos bien validados

--- imports/api/collections/Empresa/Methods.js
+++ imports/api/collections/Empresa/Methods.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/no-unresolved
 import { Meteor } from "meteor/meteor";
 import Empresa from "./Empresa";
+import Empleados from "../Empleados/Empleados";
 
 Meteor.methods({
   getEmpresa() {
-    return Empresa.find({}).fetch()[0];
+    return { ...Empresa.find().fetch()[0], cuantosEmpleados: Empleados.find().count() };
   },
   getEmpresaName() {
     return Empresa.find({}).fetch()[0].name;
--- imports/ui/pages/Empresa.js
+++ imports/ui/pages/Empresa.js
@@ -1,15 +1,8 @@
 import React, { PureComponent } from "react";
-import {
-  Paper,
-  Grid,
-  Typography,
-  TextField,
-  Button,
-  Snackbar,
-  IconButton,
-} from "@material-ui/core";
+import { Grid, Typography, TextField, Button, Snackbar, IconButton, Icon } from "@material-ui/core";
 import { Meteor } from "meteor/meteor";
 import DashboardLayout from "../layouts/DashboardLayout";
+import validator from "validator";
 
 class Empresa extends PureComponent {
   constructor(props) {
@@ -21,6 +14,7 @@ class Empresa extends PureComponent {
       RTN: "",
       CAI: "",
       open: false,
+      mensaje: "",
     };
     Meteor.call("getEmpresa", (error, result) => {
       this.setState({
@@ -38,64 +32,75 @@ class Empresa extends PureComponent {
   };
 
   render() {
-    const { name, RTN, CAI, empresa, id, open } = this.state;
+    const { name, RTN, CAI, empresa, open, mensaje } = this.state;
     return (
       <DashboardLayout>
         <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <Paper>
-              <Typography variant="body1">{`Nombre: ${empresa.name}`}</Typography>
-              <TextField
-                label="Nombre"
-                fullWidth
-                value={name}
-                onInput={e => {
-                  this.setState({ name: e.target.value });
-                }}
-              />
-            </Paper>
+          <Grid item xs={6}>
+            <Typography variant="body1">{`Nombre: ${empresa.name}`}</Typography>
+            <Typography variant="body1">{`RTN: ${empresa.RTN}`}</Typography>
+            <Typography variant="body1">{`CAI: ${empresa.CAI}`}</Typography>
+            <Typography variant="body1">{`Cuantos empleados: ${empresa.cuantosEmpleados}`}</Typography>
           </Grid>
-          <Grid item xs={4}>
-            <Paper>
-              <Typography variant="body1">{`RTN: ${empresa.RTN}`}</Typography>
-              <TextField
-                label="RTN"
-                fullWidth
-                value={RTN}
-                onInput={e => {
-                  this.setState({ RTN: e.target.value });
-                }}
-              />
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper>
-              <Typography variant="body1">{`CAI: ${empresa.CAI}`}</Typography>
-              <TextField
-                label="CAI"
-                fullWidth
-                value={CAI}
-                onInput={e => {
-                  this.setState({ CAI: e.target.value });
-                }}
-              />
-            </Paper>
+          <Grid item xs={6}>
+            <TextField
+              label="Nombre"
+              fullWidth
+              value={name}
+              onInput={e => {
+                this.setState({ name: e.target.value });
+              }}
+            />
+            <TextField
+              label="RTN"
+              fullWidth
+              value={RTN}
+              onInput={e => {
+                this.setState({ RTN: e.target.value });
+              }}
+            />
+            <TextField
+              label="CAI"
+              fullWidth
+              value={CAI}
+              onInput={e => {
+                this.setState({ CAI: e.target.value });
+              }}
+            />
           </Grid>
           <Button
             fullWidth
             variant="contained"
             color="primary"
             onClick={() => {
-              Meteor.call("updateEmpresa", { name, RTN, CAI }, (error, result) => {
-                this.setState({
-                  empresa: result,
-                  id: result._id,
-                  name: result.name,
-                  RTN: result.RTN,
-                  CAI: result.CAI,
-                  open: true,
+              let alert;
+              if (validator.isEmpty(name)) {
+                alert = "El nombre no puede estar vacio.";
+              }
+              if (validator.isEmpty(RTN)) {
+                alert = "El RTN no puede estar vacio.";
+              }
+              if (!validator.matches(RTN, /\d{4}-\d{4}-\d{6}/)) {
+                alert = "El formato del rtn es incorrecto";
+              }
+              if (validator.isEmpty(CAI)) {
+                alert = "El CAI no puede estar vacio.";
+              }
+              if (alert) {
+                this.setState({ open: true, mensaje: alert });
+              } else {
+                Meteor.call("updateEmpresa", { name, RTN, CAI }, (error, result) => {
+                  this.setState({
+                    empresa: result,
+                    id: result._id,
+                    name: result.name,
+                    RTN: result.RTN,
+                    CAI: result.CAI,
+                    open: true,
+                    mensaje: "Datos actualizados exitosamente",
+                  });
                 });
-              });
+              }
             }}>
             Actualizar
           </Button>
@@ -110,7 +115,7 @@ class Empresa extends PureComponent {
             ContentProps={{
               "aria-describedby": "message-id",
             }}
-            message={<span id="message-id">Datos Actualizados excitosamente</span>}
+            message={<span id="message-id">{mensaje}</span>}
             action={[
               <IconButton key="close" aria-label="close" color="inherit" onClick={this.handleClose}>
                 <i className="fas fa-times" />
