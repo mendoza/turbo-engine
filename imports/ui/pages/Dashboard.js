a47077fcaf79dd2344a35ef9062c6462451eaec6
carlosvhz <carlosvhz@outlook.com>
2019-12-04 19:23:02 -0600

    Bug de Varela, aun falta un detalle

--- imports/ui/pages/Dashboard.js
+++ imports/ui/pages/Dashboard.js
@@ -10,6 +10,7 @@ import {
   CartesianGrid,
   XAxis,
   YAxis,
+  ResponsiveContainer,
 } from "recharts";
 import DashboardLayout from "../layouts/DashboardLayout";
 import Orders from "../components/Orders";
@@ -50,27 +51,32 @@ class Dashboard extends PureComponent {
     return (
       <DashboardLayout Routes={[]}>
         <Grid container className="gridRoot" item xs={12} spacing={1}>
-          <Grid item xs={6}>
-            <Title>Gráfico</Title>
-            <LineChart width={500} height={230} data={data}>
-              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" />
-              <XAxis dataKey="name" />
-              <YAxis />
-            </LineChart>
+          <Grid item xs={12} sm={6}>
+            <ResponsiveContainer width="100%">
+              <div>
+                <LineChart width={500} height={230} data={data}>
+                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                </LineChart>
+              </div>
+            </ResponsiveContainer>
           </Grid>
-
-          <Grid item xs={6}>
-            <Title>Gráfico 2</Title>
-            <BarChart width={610} height={250} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
-            </BarChart>
+          <Grid item xs={12} sm={6}>
+            <ResponsiveContainer width="100%">
+              <div>
+                <BarChart width={500} height={230} data={data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="pv" fill="#8884d8" />
+                  <Bar dataKey="uv" fill="#82ca9d" />
+                </BarChart>
+              </div>
+            </ResponsiveContainer>
           </Grid>
         </Grid>
 
