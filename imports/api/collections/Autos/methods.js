33778f34e2529ae2324a42cc03f2a0cb9e2552a4
carlosvhz <carlosvhz@outlook.com>
2019-12-12 19:45:57 -0600

    Quite una tontera Jaa

--- imports/api/collections/Autos/methods.js
+++ imports/api/collections/Autos/methods.js
@@ -1,21 +1,8 @@
 import { Meteor } from "meteor/meteor";
-import Piezas from "../Piezas/Piezas";
 import Auto from "./Autos";
 
 Meteor.methods({
   addAuto(payload) {
-    const { piezas } = payload;
-    piezas.map(pieza => {
-      console.log(pieza);
-      return Piezas.update(
-        { _id: pieza._id },
-        {
-          $set: {
-            tipo: "pene",
-          },
-        }
-      );
-    });
     return Auto.insert(payload);
   },
   getAutos() {
