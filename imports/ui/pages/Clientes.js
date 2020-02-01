fa37a6141625a5d84e4e8d3a75a12fdc9b2b290c
David Mendoza <d.mendoza99@unitec.edu>
2020-02-01 09:10:20 -0600

    RTN listo para el branch

--- imports/ui/pages/Clientes.js
+++ imports/ui/pages/Clientes.js
@@ -1,43 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import validatorjs from "validator";
 import {
-  Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, Grid, Snackbar, IconButton, Table, TableRow, TableHead, TableCell, TableBody
-} from '@material-ui/core';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import { withTracker } from 'meteor/react-meteor-data';
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+  Grid,
+  Snackbar,
+  IconButton,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import { withTracker } from "meteor/react-meteor-data";
 import DashboardLayout from "../layouts/DashboardLayout";
-import Cliente from '../../api/collections/Cliente/Cliente';
+import Cliente from "../../api/collections/Cliente/Cliente";
+import MaskedTextField from "../components/MaskedTextField";
 
 class Clientes extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       showClientDialog: false,
       showSnackbar: false,
       showDeleteDialog: false,
       editId: undefined,
-      snackbarText: '',
-      Nombre: '',
-      Apellido: '',
-      RTN: '',
-      Telefono: '',
-      Telefono2: '',
-      Company: '',
-      email: '',
-    }
+      snackbarText: "",
+      Nombre: "",
+      Apellido: "",
+      RTN: "",
+      Telefono: "",
+      Telefono2: "",
+      Company: "",
+      email: "",
+    };
   }
 
   handleTextInput = (event, stateName, validator) => {
     let error;
-    if (stateName === 'email') {
+    if (stateName === "email") {
       error = !validatorjs.isEmail(event.target.value);
       if (error) {
-        error = 'Correo no válido';
+        error = "Correo no válido";
       }
     }
+    console.log(stateName, validator);
     if (validator) {
-      if (validator(event.target.value) || event.target.value === '') {
+      if (validator(event.target.value) || event.target.value === "") {
         this.setState({
           [stateName]: event.target.value,
         });
@@ -48,14 +63,12 @@ class Clientes extends Component {
         emailError: error,
       });
     }
-  }
+  };
 
   handleCreateClient = event => {
     const { emailError } = this.state;
     event.preventDefault();
-    const {
-      Nombre, Apellido, RTN, Telefono, Telefono2, Company, email, editId,
-    } = this.state;
+    const { Nombre, Apellido, RTN, Telefono, Telefono2, Company, email, editId } = this.state;
     const newClient = {
       _id: editId,
       nombre: Nombre,
@@ -68,55 +81,55 @@ class Clientes extends Component {
     };
     let methodName;
     if (editId) {
-      methodName = 'handleEditClient';
+      methodName = "handleEditClient";
     } else {
-      methodName = 'handleCreateClient';
+      methodName = "handleCreateClient";
     }
     if (emailError) {
       this.setState({
         showSnackbar: true,
-        snackbarText: 'Por favor llene el campo de Correo Electrónico',
+        snackbarText: "Por favor llene el campo de Correo Electrónico",
       });
     } else {
       Meteor.call(methodName, newClient, error => {
         if (error) {
           this.setState({
             showSnackbar: true,
-            snackbarText: 'Ha ocurrido un error al intentar guardar el cliente'
+            snackbarText: "Ha ocurrido un error al intentar guardar el cliente",
           });
         } else {
           this.setState({
             showClientDialog: false,
-            Nombre: '',
-            Apellido: '',
-            RTN: '',
-            Telefono: '',
-            Telefono2: '',
-            Company: '',
-            email: '',
+            Nombre: "",
+            Apellido: "",
+            RTN: "",
+            Telefono: "",
+            Telefono2: "",
+            Company: "",
+            email: "",
           });
         }
       });
     }
-  }
+  };
 
   handleDeleteClient = () => {
     const { editId } = this.state;
-    Meteor.call('handleDeleteClient', editId, error => {
+    Meteor.call("handleDeleteClient", editId, error => {
       if (error) {
         this.setState({
           showSnackbar: true,
-          snackbarText: 'Ha ocurrido un error al eliminar el cliente'
+          snackbarText: "Ha ocurrido un error al eliminar el cliente",
         });
       } else {
         this.setState({
           showDeleteDialog: false,
           showSnackbar: true,
-          snackbarText: 'Cliente eliminado exitosamente'
+          snackbarText: "Cliente eliminado exitosamente",
         });
       }
     });
-  }
+  };
 
   renderClientDialog = () => {
     const {
@@ -134,14 +147,15 @@ class Clientes extends Component {
     return (
       <Dialog
         open={showClientDialog}
-        onClose={() => { this.setState({ showClientDialog: false }) }}
+        onClose={() => {
+          this.setState({ showClientDialog: false });
+        }}
         aria-labelledby="form-dialog-title"
         maxWidth="md"
-        fullWidth
-        >
+        fullWidth>
         <form onSubmit={this.handleCreateClient}>
           <DialogTitle id="form-dialog-title">
-            {editId ? 'Editar ' : 'Agregar '}
+            {editId ? "Editar " : "Agregar "}
             cliente
           </DialogTitle>
           <DialogContent>
@@ -150,95 +164,116 @@ class Clientes extends Component {
                 <TextField
                   label="Nombre"
                   onInput={event => {
-                    this.handleTextInput(event, 'Nombre', text => {
-                      return validatorjs.isAlpha(text, 'es-ES')
-                    })
+                    this.handleTextInput(event, "Nombre", text => {
+                      return validatorjs.isAlpha(text, "es-ES");
+                    });
                   }}
                   value={Nombre}
                   required
                   autoFocus
                   fullWidth
-                  />
+                />
               </Grid>
               <Grid item xs={12} md={6}>
                 <TextField
                   label="Apellido"
                   onInput={event => {
-                    this.handleTextInput(event, 'Apellido', text => {
-                      return validatorjs.isAlpha(text, 'es-ES')
-                    })
+                    this.handleTextInput(event, "Apellido", text => {
+                      return validatorjs.isAlpha(text, "es-ES");
+                    });
                   }}
                   value={Apellido}
                   required
                   fullWidth
-                  />
+                />
               </Grid>
               <Grid item xs={12} md={6}>
-                <TextField
-                  label="RTN"
-                  onInput={event => {
-                    this.handleTextInput(event, 'RTN', text => {
-                      return validatorjs.isNumeric(text, { no_symbols: true });
-                    })
-                  }}
+                <MaskedTextField
+                  mask={[
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                  ]}
                   value={RTN}
-                  fullWidth
-                  />
+                  name="RTN"
+                  onChange={event => {
+                    this.handleTextInput(event, "RTN", text => {
+                      return validatorjs.isNumeric(text, { no_symbols: true });
+                    });
+                  }}
+                  label="RTN"
+                />
               </Grid>
               <Grid item xs={12} md={6}>
                 <TextField
                   label="Teléfono"
                   onInput={event => {
-                    this.handleTextInput(event, 'Telefono', text => {
+                    this.handleTextInput(event, "Telefono", text => {
                       return validatorjs.isNumeric(text, { no_symbols: true });
-                    })
+                    });
                   }}
                   value={Telefono}
                   fullWidth
                   required
-                  />
+                />
               </Grid>
               <Grid item xs={12} md={6}>
                 <TextField
                   label="Teléfono del trabajo"
                   onInput={event => {
-                    this.handleTextInput(event, 'Telefono2', text => {
+                    this.handleTextInput(event, "Telefono2", text => {
                       return validatorjs.isNumeric(text, { no_symbols: true });
-                    })
+                    });
                   }}
                   value={Telefono2}
                   required
                   fullWidth
-                  />
+                />
               </Grid>
               <Grid item xs={12} md={6}>
                 <TextField
                   label="Compañía"
-                  onInput={event => { this.handleTextInput(event, 'Company') }}
+                  onInput={event => {
+                    this.handleTextInput(event, "Company");
+                  }}
                   value={Company}
                   required
                   fullWidth
-                  />
+                />
               </Grid>
               <Grid item xs={12} md={6}>
                 <TextField
                   label="Correo Electrónico"
-                  onInput={event => { this.handleTextInput(event, 'email') }}
+                  onInput={event => {
+                    this.handleTextInput(event, "email");
+                  }}
                   value={email}
                   error={!!emailError}
-                  helperText={emailError || ''}
+                  helperText={emailError || ""}
                   required
                   fullWidth
-                  />
+                />
               </Grid>
             </Grid>
           </DialogContent>
           <DialogActions>
             <Button
-              onClick={() => { this.setState({ showClientDialog: false }) }}
+              onClick={() => {
+                this.setState({ showClientDialog: false });
+              }}
               color="primary"
-              variant="contained"
-              >
+              variant="contained">
               Cancelar
             </Button>
             <Button color="primary" variant="contained" type="submit">
@@ -248,18 +283,19 @@ class Clientes extends Component {
         </form>
       </Dialog>
     );
-  }
+  };
 
   renderDeleteDialog = () => {
     const { showDeleteDialog } = this.state;
     return (
       <Dialog
         open={showDeleteDialog}
-        onClose={() => { this.setState({ showDeleteDialog: false }) }}
+        onClose={() => {
+          this.setState({ showDeleteDialog: false });
+        }}
         aria-labelledby="form-dialog-title"
         maxWidth="sm"
-        fullWidth
-        >
+        fullWidth>
         <DialogTitle id="form-dialog-title">
           ¿Está seguro que desea eliminar este cliente?
         </DialogTitle>
@@ -272,10 +308,11 @@ class Clientes extends Component {
         </DialogContent>
         <DialogActions>
           <Button
-            onClick={() => { this.setState({ showDeleteDialog: false }) }}
+            onClick={() => {
+              this.setState({ showDeleteDialog: false });
+            }}
             color="primary"
-            variant="contained"
-            >
+            variant="contained">
             Cancelar
           </Button>
           <Button color="primary" variant="contained" onClick={this.handleDeleteClient}>
@@ -284,7 +321,7 @@ class Clientes extends Component {
         </DialogActions>
       </Dialog>
     );
-  }
+  };
 
   renderClientTable = () => {
     const { clients } = this.props;
@@ -344,11 +381,10 @@ class Clientes extends Component {
                               Telefono: client.telefono,
                               Telefono2: client.telefonoTrabajo,
                               Company: client.compania,
-                              email: client.email
+                              email: client.email,
                             });
                           }}
-                          aria-label="centered"
-                          >
+                          aria-label="centered">
                           <i className="fas fa-pen" />
                         </ToggleButton>
                         <ToggleButton
@@ -358,9 +394,8 @@ class Clientes extends Component {
                             this.setState({
                               editId: client._id,
                               showDeleteDialog: true,
-                            })
-                          }}
-                          >
+                            });
+                          }}>
                           <i className="fas fa-trash" />
                         </ToggleButton>
                       </ToggleButtonGroup>
@@ -374,7 +409,7 @@ class Clientes extends Component {
         </TableBody>
       </Table>
     );
-  }
+  };
 
   renderSnackbar = () => {
     const { showSnackbar, snackbarText } = this.state;
@@ -386,9 +421,9 @@ class Clientes extends Component {
         }}
         open={showSnackbar}
         autoHideDuration={6000}
-        onClose={
-          () => { this.setState({ showSnackbar: false }) }
-        }
+        onClose={() => {
+          this.setState({ showSnackbar: false });
+        }}
         ContentProps={{
           "aria-describedby": "message-id",
         }}
@@ -398,25 +433,27 @@ class Clientes extends Component {
             key="close"
             aria-label="close"
             color="inherit"
-            onClick={() => { this.setState({ showSnackbar: false }) }}
-            >
+            onClick={() => {
+              this.setState({ showSnackbar: false });
+            }}>
             <i className="fas fa-times" />
           </IconButton>,
         ]}
-        />
+      />
     );
-  }
+  };
 
   render() {
     return (
-      <DashboardLayout style={{ height: '100vh' }}>
+      <DashboardLayout style={{ height: "100vh" }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Button
               variant="contained"
               color="primary"
-              onClick={() => { this.setState({ showClientDialog: true, editId: undefined }) }}
-              >
+              onClick={() => {
+                this.setState({ showClientDialog: true, editId: undefined });
+              }}>
               Agregar Cliente
             </Button>
           </Grid>
@@ -428,14 +465,14 @@ class Clientes extends Component {
         {this.renderSnackbar()}
         {this.renderDeleteDialog()}
       </DashboardLayout>
-    )
+    );
   }
 }
 
 export default withTracker(() => {
-  Meteor.subscribe('clientes.all');
+  Meteor.subscribe("clientes.all");
   const clients = Cliente.find().fetch();
   return {
     clients: clients && clients.reverse(),
-  }
+  };
 })(Clientes);
