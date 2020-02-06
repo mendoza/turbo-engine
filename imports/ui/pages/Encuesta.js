77ec5d0c121b87d57c57564d0c0c6b5f2ba38396
carlosvhz <carlosvhz@outlook.com>
2020-02-06 16:57:34 -0600

    Detalles

--- imports/ui/pages/Encuesta.js
+++ imports/ui/pages/Encuesta.js
@@ -1,48 +1,50 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import StarIcon from "@material-ui/icons/StarBorder";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Kike Autos
-      </Link>
-      {' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none",
     },
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
   toolbar: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
   },
   toolbarTitle: {
     flexGrow: 1,
@@ -55,12 +57,12 @@ const useStyles = makeStyles(theme => ({
   },
   cardHeader: {
     backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[700] : theme.palette.grey[200],
+      theme.palette.type === "dark" ? theme.palette.grey[700] : theme.palette.grey[200],
   },
   cardEncuestilla: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
     marginBottom: theme.spacing(2),
   },
   footer: {
@@ -68,7 +70,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingTop: theme.spacing(6),
       paddingBottom: theme.spacing(6),
     },
@@ -77,36 +79,32 @@ const useStyles = makeStyles(theme => ({
 
 const tiers = [
   {
-    title: 'Califica',
-    price: '0',
-    description: ['Insertar Emoji',],
-    buttonText: 'Mal Servicio',
-    buttonVariant: 'outlined',
+    title: "Califica",
+    price: "0",
+    description: [<i style={{ fontSize: "24px" }} className="far fa-angry" />],
+    buttonText: "Mal Servicio",
+    buttonVariant: "outlined",
   },
   {
-    title: 'Califica',
-    subheader: 'El buen servicio', 
-    price: '5',
-    description: [
-        'Insertar Emoji',
-    ],
-    buttonText: 'Excelente',
-    buttonVariant: 'contained',
+    title: "Califica",
+    subheader: "El buen servicio",
+    price: "5",
+    description: [<i style={{ fontSize: "24px" }} className="far fa-smile" />],
+    buttonText: "Excelente",
+    buttonVariant: "contained",
   },
   {
-    title: 'Califica',
-    price: '3',
-    description: [
-      'Insertar Emoji',
-    ],
-    buttonText: 'Bien / Regular',
-    buttonVariant: 'outlined',
+    title: "Califica",
+    price: "3",
+    description: [<i style={{ fontSize: "24px" }} className="far fa-meh" />],
+    buttonText: "Bien / Regular",
+    buttonVariant: "outlined",
   },
 ];
 const footers = [
   {
-    title: 'Empresa',
-    description: ['Equipo', 'Historia', 'Contactanos', 'Localidad'],
+    title: "Empresa",
+    description: ["Equipo", "Historia", "Contactanos", "Localidad"],
   },
   /*
   {
@@ -119,8 +117,8 @@ const footers = [
   },
   */
   {
-    title: 'Legal',
-    description: ['Politicas de Privacidad', 'Terminos de Uso'],
+    title: "Legal",
+    description: ["Politicas de Privacidad", "Terminos de Uso"],
   },
 ];
 
@@ -132,23 +130,11 @@ export default function Encuestilla() {
       <CssBaseline />
       <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Kikes Auto&apos;s
-          </Typography>
           <nav>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-              Preguntas
-            </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-              Sugerenciass
-            </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-              "LOGO AQUI"
-            </Link>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+              Kikes Auto&apos;s
+            </Typography>
           </nav>
-          <Button href="#" color="primary" variant="outlined" className={classes.link}>
-            Login
-          </Button>
         </Toolbar>
       </AppBar>
       {/* Hero unit */}
@@ -157,8 +143,8 @@ export default function Encuestilla() {
           Encuesta
         </Typography>
         <Typography variant="h5" align="center" color="textSecondary" component="p">
-          Gracias por ser parte de Kike&apos;s Autos, 
-          queremos mejorar y tu opinion cuenta. Califica nuestro servicio.
+          Gracias por ser parte de Kike&apos;s Autos, queremos mejorar y tu opinion cuenta. Califica
+          nuestro servicio.
         </Typography>
       </Container>
       {/* End hero unit */}
@@ -166,21 +152,20 @@ export default function Encuestilla() {
         <Grid container spacing={5} alignItems="flex-end">
           {tiers.map(tier => (
             // Enterprise card is full width at sm breakpoint
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+            <Grid item key={tier.title} xs={12} sm={tier.title === "Enterprise" ? 12 : 6} md={4}>
               <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  action={tier.title === "Pro" ? <StarIcon /> : null}
                   className={classes.cardHeader}
-                  />
+                />
                 <CardContent>
                   <div className={classes.cardEncuestilla}>
                     <Typography component="h2" variant="h3" color="textPrimary">
-                      ☆
-                      {tier.price}
+                      ☆{tier.price}
                     </Typography>
                     <Typography variant="h6" color="textSecondary">
                       /pts
@@ -202,6 +187,31 @@ export default function Encuestilla() {
               </Card>
             </Grid>
           ))}
+        </Grid>
+        <Grid container spacing={5} alignItems="flex-end">
+          <FormControl fullWidth className={classes.margin}>
+            <InputLabel htmlFor="standard-adornment-amount">
+              Agrege sugerencias o un comentario
+            </InputLabel>
+            <Input
+              id="standard-adornment-amount"
+              multiline
+              /* El valor value es el que se tiene que guardar */
+              // value={values.amount}}
+              // startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            />
+          </FormControl>
+        </Grid>
+        <Grid container spacing={5} alignItems="flex-end">
+          {/*
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Terminar encuesta
+          </Button>*/}
         </Grid>
       </Container>
       {/* Footer */}
