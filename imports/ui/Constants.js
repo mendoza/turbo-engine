e26c4ab7af7574c715c0bd7147403f62bddb81b3
David Mendoza <d.mendoza99@unitec.edu>
2020-03-01 14:00:44 -0600

    ya esta esta branch

--- imports/ui/Constants.js
+++ imports/ui/Constants.js
@@ -2,6 +2,8 @@ export default {};
 
 export const Estados = ["Reparando", "En reparacion", "En Venta", "Vendido"];
 
-export const Traccion = ["Trasera", "Delantera", "4x4"];
+export const Traccion = ["Trasera", "Delantera", "Doble"];
 
 export const Transmision = ["Manual", "Automatico", "Mixta"];
+
+export const Tipo = ["Camioneta", "Turismo", "Deportivo"];
