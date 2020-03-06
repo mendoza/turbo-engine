bc7a0361209c0bc9d9185aeca5545e31212da208
leonard1on <leonardborj@gmail.com>
2020-03-06 12:50:46 -0600

    Redirect

--- imports/ui/pages/ListTickets.js
+++ imports/ui/pages/ListTickets.js
@@ -8,13 +8,21 @@ import {
   TableHead,
   TableCell,
   TableBody,
+  Button,
 } from "@material-ui/core";
 import { Meteor } from "meteor/meteor";
 import { withTracker } from "meteor/react-meteor-data";
+import { Redirect } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import Reportes from "../../api/collections/Reportes/Reportes";
+import Title from "../components/Title";
 
 class ListTickets extends Component {
+  constructor(props){
+    super(props);
+    this.state= { shouldRedirect: false, pathname: "", shouldRender: false,};
+  }
+
   renderTicketsTable = () => {
     const { reportes } = this.props;
     return (
@@ -37,18 +45,11 @@ class ListTickets extends Component {
                 <TableRow key={row.name}>
                   <TableCell align="left">{row.prioridad}</TableCell>
                   <TableCell align="left">{`${date.toLocaleDateString()}`}</TableCell>
-                  <TableCell align="left">{`${
-                    Meteor.users.findOne({ _id: row.empleado }).profile.firstName
-                  }`}</TableCell>
+                  <TableCell align="left">{`${Meteor.users.findOne({ _id: row.empleado }).profile.firstName}`}</TableCell>
                   <TableCell align="left">{row.tipo}</TableCell>
                   <TableCell align="left">{row.comentario}</TableCell>
                   <TableCell align="left">
-                    {
-                      <i
-                        className={`fas fa-${row.abierto ? "check" : "times"}`}
-                        style={{ color: row.abierto ? "green" : "red" }}
-                      />
-                    }
+                    <i className={`fas fa-${row.abierto ? "check" : "times"}`} style={{ color: row.abierto ? "green" : "red" }}/>
                   </TableCell>
                 </TableRow>
               );
@@ -92,13 +93,28 @@ class ListTickets extends Component {
   };
 
   render() {
+    const {shouldRedirect, pathname} = this.state;
     return (
       <DashboardLayout style={{ height: "100vh" }}>
+        <Title>Listado de Tickets</Title>
+        <Grid container spacing={2} justify="left">
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                this.setState({ pathname: "crearreportes", shouldRedirect: true });
+              }}>
+              Agregar Ticket
+            </Button>
+          </Grid>
+        </Grid>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             {this.renderTicketsTable()}
           </Grid>
         </Grid>
+        {shouldRedirect ? <Redirect to={pathname} /> : null}
       </DashboardLayout>
     );
   }
